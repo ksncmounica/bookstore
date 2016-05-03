7c00366c7a1617a97815b857f3c22d0e4b130f39
ksncmounica <mounica.kokkerlapati@mtuity.com>
2016-05-03 23:00:11 +0530

    second commit

--- public/javascripts/bookdetails.js
+++ public/javascripts/bookdetails.js
@@ -4,15 +4,12 @@ function createTable(){
      var info={};
       $.get("http://localhost:3000/books/getbooks",
         function(info,status){
-          console.log(info);
-          alert("Info: " + info + "\nStatus: " + status);
-          console.log(info.length);
           $.each(info, function(i){
             var str='<tr>';
-            str += '<td>' + this.bookisbn + '<td>';
-            str += '<td>' + this.bookname + '<td>';
-            str += '<td>' + this.authorid + '<td>';
-            str += '<td>' + this.authorname + '<td>';
+            str += '<td>' + this.bookisbn + '</td>';
+            str += '<td>' + this.bookname + '</td>';
+            str += '<td>' + this.authorid + '</td>';
+            str += '<td>' + this.authorname + '</td>';
             str += '</tr>';
             $('#results').append(str);
           });
